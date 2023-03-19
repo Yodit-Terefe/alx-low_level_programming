68b583a3678ac7c8e70b17b7871f970e21243273
Yodit-Terefe <yoditterefe12@gmail.com>
2023-03-19 05:00:21 -0700

    uoppp

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,7 @@
 /**
 * main -prints the last digit of a randomly generated number
 * and whether it is greater than 5,less than 6, or or 0.
+*
 * Return: Alwayes 0.
 */
 int main(void)
@@ -17,7 +18,7 @@ printf("last digit of %d is %d and is greater than 5\n",n,n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) !=0)
 {
-printf("last digit of %d is %d and is less rhan 6 and not 0\n");
+printf("last digit of %d is %d and is less rhan 6 and not 0\n",n,n % 10);
 }
 else
 {
