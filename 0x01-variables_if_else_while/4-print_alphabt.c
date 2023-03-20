6fd5f849cdc9f597c6a0e4462969844e8caa7bae
Yodit-Terefe <yoditterefe12@gmail.com>
2023-03-19 21:59:07 -0700

    ioo

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * main -prints the alphabet without q and e.
+ * main - Prints the alphabet without q and e.
  *
- * Return: 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-         int i;
-         for (i = 97; i < 123; i++)
-         {  
-                  if (i != 101 && i != 113)
-                  {       
-                           putchar(i);
-		  } 
-	 }    	  
-         putchar('\n');
-         return (0);
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
